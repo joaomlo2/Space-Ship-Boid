5f5eb437d7feb6c2332876d65bc2b60b8a8c8cd7
joaomlo <joaomlo2@gmail.com>
2017-03-05 18:23:49 +0000

    First Commit

---
+++ Assets/Scripts/PlayerShipController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerShipController : MonoBehaviour
+{
+    public float Speed=3.0f;
+    public float Acceleration=0.0f;
+
+    public float DebugLineLength = 3.0f;
+
+	void Update ()
+    {
+
+	}
+
+    void ProcessMovement()
+    {
+        Vector3 movementToApply=Vector3.zero;
+    }
+
+    void OnDrawGizmos()
+    {
+        DrawForwardVector();
+    }
+
+    void DrawForwardVector()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawLine(transform.position, transform.position + (DebugLineLength * transform.forward));
+    }
+}
