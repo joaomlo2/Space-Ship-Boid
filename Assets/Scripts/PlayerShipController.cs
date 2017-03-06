7ae19d8dcc156f6c8f5b4825edffe2f0edaccc6c
João Oliveira <joaomlo2@gmail.com>
2017-03-06 16:41:33 +0000

    Movimento da nave do jogador está funcional

--- Assets/Scripts/PlayerShipController.cs
+++ Assets/Scripts/PlayerShipController.cs
@@ -1,23 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Permissions;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class PlayerShipController : MonoBehaviour
 {
-    public float Speed=3.0f;
-    public float Acceleration=0.0f;
+    public float Speed;
+    public float TurningStrength;
 
     public float DebugLineLength = 3.0f;
 
 	void Update ()
     {
-
+        ProcessMovement();
 	}
 
     void ProcessMovement()
     {
         Vector3 movementToApply=Vector3.zero;
+        //Speed
+        if (Input.GetKey(KeyCode.A))
+        {
+            Speed += 0.1f*Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            Speed -= 0.1f * Time.deltaTime;
+            if (Speed < 0)
+                Speed = 0;
+        }
+        //Rotation
+        if (Input.GetKey(KeyCode.I))
+        {
+            transform.Rotate(TurningStrength, 0.0f,0.0f);
+        }
+        if (Input.GetKey(KeyCode.K))
+        {
+            transform.Rotate(-TurningStrength, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.J))
+        {
+            transform.Rotate(0.0f, -TurningStrength, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            transform.Rotate(0.0f, TurningStrength, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.U))
+        {
+            transform.Rotate(0.0f, 0.0f, TurningStrength);
+        }
+        if (Input.GetKey(KeyCode.O))
+        {
+            transform.Rotate(0.0f, 0.0f, -TurningStrength);
+        }
+        transform.position += transform.forward*Speed;
     }
 
     void OnDrawGizmos()
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    private GameObject _playerShip;
+    public GameObject SpeedMeter;
+
+    void Awake()
+    {
+        SpeedMeter = transform.FindChild("AccelerationIndicator").gameObject;
+        _playerShip = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        SpeedMeter.GetComponent<Text>().text = "Speed: " +
+                                                      _playerShip.GetComponent<PlayerShipController>().Speed;
+    }
+}
